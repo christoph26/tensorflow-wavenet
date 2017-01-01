519fb7cec05e9991d5268410601108f816075215
sennluzi <luzis@student.ethz.ch>
2017-01-01 16:47:27 +0100

    corrected no audio files error

--- wavenet/audio_reader.py
+++ wavenet/audio_reader.py
@@ -115,7 +115,7 @@ class AudioReader(object):
         # TODO Find a better way to check this.
         # Checking inside the AudioReader's thread makes it hard to terminate
         # the execution of the script, so we do it in the constructor for now.
-        if not find_files(audio_dir, pattern="*.npz"):
+        if not find_files(audio_dir, pattern="*_pca.h5"):
             raise ValueError("No audio files found in '{}'.".format(audio_dir))
 
     def dequeue(self, num_elements):
